5c50ff8864e02a9fa8671de59879702460a000a0
Douglas <48629993+DouglasKrouth@users.noreply.github.com>
2024-01-21 10:16:28 -0600

    Update telescope.lua to exclude .venv/nodemodule patterns

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,4 +5,19 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+local telescope = require('telescope')
+telescope.setup {
+pickers = {
+find_files = {
+hidden = true
+}
+},
+defaults = {
+    file_ignore_patterns = {
+      "node_modules",
+      ".venv"
+    }
+  }
+}
+
 
